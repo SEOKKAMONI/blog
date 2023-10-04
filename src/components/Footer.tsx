318ea1ccf48a4acfc56ecce81789bfdddf8fda03
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-04 23:47:21 +0900

    feat: add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
 import styled from "@emotion/styled";
 
 const Footer = () => {
-  return <StyledFooter>sa</StyledFooter>;
+  return (
+    <StyledFooter>
+      © {new Date().getFullYear()}. Kimseokjin all rights reserved.
+    </StyledFooter>
+  );
 };
 
 export default Footer;
 
-const StyledFooter = styled.footer``;
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  color: ${({ theme }) => theme.colors.gray700};
+`;
